c22b3d97083e51eebf7ad2f88a138a53ab2eed16
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2024-06-03 10:53:07 +0700

    menuliskna loop

--- iofil/iofil.cpp
+++ iofil/iofil.cpp
@@ -10,5 +10,25 @@ int main() {
 	// membuka file dalam mode menulis
 	ofstream outfile;
 	//menunjukan ke sebuah nama file
+	outfile.open("contohfile.txt");
+
+	cout << ">= menulis file,\'q\'untuk keluar" << endl;
+
+	// unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		// mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		// loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q") break;
+		//menuliskan dan memesukan nilai dari'baris'ke dalam file
+		outfile << baris << endl;
+	}
+	
+
 }
+
+
+
+
 
